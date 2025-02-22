28612e55fe3fe63324bebd3fa06dfc757a165e27
Adarsh <adarsht00001@gmail.com>
2025-02-22 19:15:31 +0530

    ✨ wezterm refactored to older version

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,18 +2,20 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
--- Wayland false is required to launch in Hyprland 
+-- Wayland false is required to launch in Hyprland
 config.enable_wayland = false
--- config.font = wezterm.font("JetBrains Mono")
--- config.font_size = 10
--- 
--- config.enable_tab_bar = false
--- config.window_decorations = "RESIZE"
--- 
--- 
--- 
--- color_scheme = "tokyonight",
--- automatically_reload_config = true,
+
+
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 10
+config.enable_tab_bar = false
+
+config.window_padding = {
+	bottom = 0,
+}
+
+config.window_background_opacity = 0.50
+config.macos_window_background_blur = 400
 
 
 return config
