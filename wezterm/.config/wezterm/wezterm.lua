f6335c89e65324e4de4cf0253f7f44d14de847b7
adarsht0001 <adarsht00001@gmail.com>
2025-06-22 14:01:25 +0530

    ✨ update: adjust font size and window background opacity in wezterm configuration

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,17 +6,17 @@ local config = wezterm.config_builder()
 config.enable_wayland = false
 
 config.font = wezterm.font("JetBrains Mono")
-config.font_size = 10
+config.font_size = 12
 config.enable_tab_bar = false
 
 config.window_padding = {
 	bottom = 0,
 	right = 0,
 	left = 0,
-	top = 5 
+	top = 5
 }
 
-config.window_background_opacity = 0.50
+config.window_background_opacity = 0.60
 config.macos_window_background_blur = 400
 
 config.window_close_confirmation = "NeverPrompt"
